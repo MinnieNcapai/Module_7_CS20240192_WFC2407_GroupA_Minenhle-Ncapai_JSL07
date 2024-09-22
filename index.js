08fa98c5005d520518cf3fa1a083aef1f545c7fc
Minenhle Ncapai <minipai64@gmail.com>
2024-09-22 15:37:40 +0200

    Add toggle button for dark and light theme with matching styles.

--- index.js
+++ index.js
@@ -3,7 +3,28 @@ document.addEventListener('DOMContentLoaded', function () {
     const modal = document.getElementById('modal');
     const certificateContent = document.getElementById('certificateContent');
     const closeModal = document.querySelector('.close');
+
+    // Get the toggle button element by its ID
+    const toggleButton = document.getElementById('themeToggle');
+
+    // Function to toggle between dark and light themes
+    const toggleTheme = () => {
+      // Toggle 'dark-theme' class on the body for dark mode
+      document.body.classList.toggle('dark-theme');
+      // Apply dark-theme class to the form as well
+      cardForm.classList.toggle('dark-theme'); 
+      // Apply dark-theme class to modal
+    modal.classList.toggle('dark-theme'); 
+  };
   
+// Add click listener to toggle button for theme change
+  toggleButton.addEventListener('click', toggleTheme);
+  // Close the modal when the close button is clicked
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none'; // Hide the modal
+  });
+
+
     // Hide the modal initially
     modal.style.display = 'none';
   
@@ -52,4 +73,5 @@ document.addEventListener('DOMContentLoaded', function () {
       modal.style.display = 'none'; 
     });
   });
+  
   
